6be76a0613537256a1a414897b6186823e40571b
plok74122 <plok74122@gmail.com>
2016-11-03 10:42:45 +0800

    edit update destroy actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,4 +20,23 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(params.require(:event).permit(:name,:description))
+      redirect_to :action => :show , :id => @event
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to :action => :index
+  end
 end
