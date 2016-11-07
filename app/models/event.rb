46a74886acec6d5d3c08dd7cf51ab45dfa358994
plok74122 <plok74122@gmail.com>
2016-11-07 10:30:35 +0800

    add attendees model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   validates_presence_of :name , :description
+
+  has_many :attendees
 end
---
+++ db/migrate/20161107020152_create_attendees.rb
@@ -0,0 +1,10 @@
+class CreateAttendees < ActiveRecord::Migration[5.0]
+  def change
+    create_table :attendees do |t|
+      t.string :username
+      t.integer :event_id
+      t.timestamps
+    end
+    add_index :attendees , :event_id
+  end
+end
